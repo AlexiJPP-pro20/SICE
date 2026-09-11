0cab2ab6f205070c9c8894658525b45a9fc513d6
AlexiJPP-pro20 <alexxipalaxio@gmail.com>
2026-09-11 19:28:59 -0400

    fix: lazy init of ASGIMiddleware for uwsgi fork compatibility

--- wsgi.py
+++ wsgi.py
@@ -10,4 +10,10 @@ if project_home not in sys.path:
 from a2wsgi import ASGIMiddleware
 from api import app
 
-application = ASGIMiddleware(app)
+_asgi_app = None
+
+def application(environ, start_response):
+    global _asgi_app
+    if _asgi_app is None:
+        _asgi_app = ASGIMiddleware(app)
+    return _asgi_app(environ, start_response)
